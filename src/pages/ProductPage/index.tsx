842d0541f77555b796cbe091e9845a428f335de2
glennnnnn <ljlsjcnh649692327@gmail.com>
2024-09-07 21:16:06 +0800

    070924

--- src/pages/ProductPage/index.tsx
+++ src/pages/ProductPage/index.tsx
@@ -1,13 +1,37 @@
 import React, { useState, useEffect } from 'react';
-import { Table, Button } from 'antd';
+import { Table, Button, Card } from 'antd';
+import type { GetProp, TableProps } from 'antd';
 import { useNavigate } from 'react-router-dom';
+import type { SorterResult } from 'antd/es/table/interface';
+import {
+  useGetProductListWithParamsQuery,
+} from "@/redux/services/productApi";
 
+import {
+  useGetProductCategoryListQuery,
+} from "@/redux/services/productApi";
+import './index.scss';
+
+type ColumnsType<T extends object = object> = TableProps<T>['columns'];
+type TablePaginationConfig = Exclude<GetProp<TableProps, 'pagination'>, boolean>;
 
 interface Product {
   productId: string;
   productName: string;
-  price: number;
-  image: string; // URL or Base64 string of the image
+  productPrice: number;
+  productImage: string; // URL or Base64 string of the image
+}
+
+interface ProductCategory {
+  productCategoryId: string;
+  productCategoryName: string;
+}
+
+interface TableParams {
+  pagination?: TablePaginationConfig;
+  sortField?: SorterResult<any>['field'];
+  sortOrder?: SorterResult<any>['order'];
+  filters?: Parameters<GetProp<TableProps, 'onChange'>>[1];
 }
 
 const ProductListPage: React.FC = () => {
@@ -21,18 +45,77 @@ const ProductListPage: React.FC = () => {
     navigate('/productDetailPage'); // Replace with your actual route path
   };
 
+  const handleTableChange: TableProps<Product>['onChange'] = (pagination, filters, sorter) => {
+    setTableParams({
+      pagination,
+      filters,
+      sortOrder: Array.isArray(sorter) ? undefined : sorter.order,
+      sortField: Array.isArray(sorter) ? undefined : sorter.field,
+    });
+
+    // // `dataSource` is useless since `pageSize` changed
+    // if (pagination.pageSize !== tableParams.pagination?.pageSize) {
+    //   setData([]);
+    // }
+  };
+  const [tableParams, setTableParams] = useState<TableParams>({
+    pagination: {
+      current: 1,
+      pageSize: 10,
+    },
+  });
+
+  const { data: baseProductListData, isLoading: productListDataLoading } = useGetProductListWithParamsQuery(tableParams)
+  const { data: baseProductCategoryListData, isLoading: productCategoryListDataLoading } = useGetProductCategoryListQuery()
+  const [productList, setProductList] = useState<Product[]>([])
+  const [productCategoryList, setProductCategoryList] = useState<ProductCategory[]>([])
+
+
+
+  useEffect(() => {
+    //console.log("useEffect called", { baseProductListData, tableParams });
+    // while (isLoading) {
+    //   console.log("Loading...");
+    // }
+    if (baseProductListData) {
+      //console.log(baseProductListData.data)
+      setProductList(baseProductListData.data.productResultList);
+      setProductCategoryList(baseProductCategoryListData.data);
+      setTableParams({
+        ...tableParams,
+        pagination: {
+          ...tableParams.pagination,
+          total: baseProductListData.data.count,
+          // 200 is mock data, you should read it from server
+          // total: data.totalCount,
+        },
+      });
+      console.log(baseProductListData.data.count)
+    }
+
+  }, [
+    productListDataLoading,
+    productCategoryListDataLoading,
+    tableParams.pagination?.current,
+    tableParams.pagination?.pageSize,
+    tableParams?.sortOrder,
+    tableParams?.sortField,
+    JSON.stringify(tableParams.filters),
+  ]);
+
+
   const dataSource: Product[] = [
     {
       productId: '7225017727965073408',
       productName: 'Product 1',
-      price: 100,
-      image: 'https://via.placeholder.com/150', // Example image URL
+      productPrice: 100,
+      productImage: 'https://via.placeholder.com/150', // Example image URL
     },
     {
       productId: '7225020561393586176',
       productName: 'Product 2',
-      price: 150,
-      image: 'https://via.placeholder.com/150', // Example image URL
+      productPrice: 150,
+      productImage: 'https://via.placeholder.com/150', // Example image URL
     },
     // Add more products as needed
   ];
@@ -40,7 +123,7 @@ const ProductListPage: React.FC = () => {
   const columns = [
     {
       title: 'Image',
-      dataIndex: 'image',
+      dataIndex: 'productImage',
       key: 'image',
       render: (text: string) => <img src={text} alt="Product" style={{ width: 50, height: 50 }} />, // Adjust size as needed
     },
@@ -65,16 +148,25 @@ const ProductListPage: React.FC = () => {
     },
   ];
   // Add more columns as needed
-  return <>
-    <Button type="primary" onClick={handleAddProductButtonClick}>
-      Go to Target Page
-    </Button>
-    <Button type="primary" onClick={handleProductDetailPageClick}>
-      Go to Target Page
-    </Button>
-    <Table dataSource={dataSource} columns={columns} />;
-
-  </>
+  return <div className="page-layout">
+    <Card style={{ height: '5%' }}>
+      <Button type="primary" onClick={handleAddProductButtonClick}>
+        Go to Target Page
+      </Button>
+      <Button type="primary" onClick={handleProductDetailPageClick}>
+        Go to Target Page
+      </Button>
+    </Card>
+    <div className="table-container">
+      <Table
+        // style={{ height: '90%' }}
+        dataSource={productList}
+        columns={columns}
+        onChange={handleTableChange}
+        pagination={tableParams.pagination}
+      />
+    </div>
+  </div>
 
 }
 
--- src/redux/services/productApi.tsx
+++ src/redux/services/productApi.tsx
@@ -1,5 +1,19 @@
 import { createApi } from "@reduxjs/toolkit/query/react";
 import { fetchBaseQueryByAxios, axiosBaseQueryWithBigInt, axiosPostWithMultiPartQuery } from "../api/fetchBaseQueryHelper";
+import type { SorterResult } from 'antd/es/table/interface';
+import type { GetProp, TableProps } from 'antd';
+import qs from 'qs';
+
+type ColumnsType<T extends object = object> = TableProps<T>['columns'];
+type TablePaginationConfig = Exclude<GetProp<TableProps, 'pagination'>, boolean>;
+
+interface TableParams {
+  pagination?: TablePaginationConfig;
+  sortField?: SorterResult<any>['field'];
+  sortOrder?: SorterResult<any>['order'];
+  filters?: Parameters<GetProp<TableProps, 'onChange'>>[1];
+}
+
 
 export const productApi = createApi({
   //equals to the name when we user createSlice
@@ -20,10 +34,25 @@ export const productApi = createApi({
       query: (productId) =>
         `/productService/product/singleProductWithImg?productId=${productId}`,
     }),
+    getProductListWithParams: builder.query<any, TableParams>({
+      query: (tableParams) => {
+        //const queryString = qs.stringify(tableParams, { addQueryPrefix: true });
+        const queryString = "?pageSize=" + tableParams.pagination?.pageSize + "&currentPageIndex=" + tableParams.pagination?.current
+        console.log()
+        return `/productService/product/productListWithParams${queryString}`;
+      }
+
+    }),
+    getProductCategoryList: builder.query<any, void>({
+      query: () =>
+        `/productCategory/allProductCategory`,
+    }),
   })
 })
 
 export const {
   useSendNewProductWithFileMutation,
-  useGetSingleProductWithImgQuery
+  useGetSingleProductWithImgQuery,
+  useGetProductListWithParamsQuery,
+  useGetProductCategoryListQuery
 } = productApi;
